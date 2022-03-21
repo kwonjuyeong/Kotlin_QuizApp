c38edb89ba0a7b9041e1eb3bda333c10ed7450d0
kwonjuyoung <20161527@vision.hoseo.edu>
2022-03-21 23:17:57 +0900

    질문 답안 선택 기능 구현

--- app/src/main/java/com/example/kotlinquizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/kotlinquizapp/QuizQuestionsActivity.kt
@@ -1,13 +1,24 @@
 package com.example.kotlinquizapp
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
+import org.w3c.dom.Text
 
-class QuizQuestionsActivity : AppCompatActivity() {
+class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var mCurrentPosition: Int = 1
+    private var mQuestionList:ArrayList<Question>?=null
+    private var mSelectedOptionPosition : Int = 0
+
 
     private var progressBar: ProgressBar?=null
     private var tvProgress: TextView? = null
@@ -17,9 +28,9 @@ class QuizQuestionsActivity : AppCompatActivity() {
     private var tvOptionTwo:TextView? = null
     private var tvOptionThree:TextView? = null
     private var tvOptionFour:TextView? = null
+    private var btnSubmit : Button ?=null
 
     //메모 : view binding 사용해서 더 쉽게 만들어보기
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
@@ -32,27 +43,107 @@ class QuizQuestionsActivity : AppCompatActivity() {
         tvOptionTwo = findViewById(R.id.tv_option_two)
         tvOptionThree = findViewById(R.id.tv_option_three)
         tvOptionFour = findViewById(R.id.tv_option_four)
+        btnSubmit = findViewById(R.id.btn_submit)
+
+        tvOptionOne?.setOnClickListener (this)
+        tvOptionTwo?.setOnClickListener (this)
+        tvOptionThree?.setOnClickListener (this)
+        tvOptionFour?.setOnClickListener (this)
+        btnSubmit?.setOnClickListener(this)
+
+        mQuestionList = Constants.getQuestions()
+
+        setQuestion()
+
+        defaultOptionsView()
+
+    }
 
 
-        val questionsList = Constants.getQuestions()
-        Log.e("Questions Size", "${questionsList.size}")
+    //main에 있던 코드를 선택 - 리팩터 - Function으로 함수를 만들어줘서 간단하게 사용할 수 있게 만듬.
+    //문제를 세팅해주는 함수
+    private fun setQuestion() {
 
+        val question: Question? = mQuestionList!![mCurrentPosition - 1]
 
-        for (i in questionsList) {
-            Log.e("Questions", i.question)
-        }
-        val currentPosition = 1
-        val question: Question? = questionsList[currentPosition - 1]
-
-        progressBar?.progress = currentPosition
-        tvProgress?.text = "$currentPosition" + "/" + progressBar?.getMax()
-
-        // Now set the current question and the options in the UI
+        progressBar?.progress = mCurrentPosition
+        tvProgress?.text = "$mCurrentPosition/${progressBar?.max}"
         tvQuestion?.text = question!!.question
         ivImage?.setImageResource(question.image)
         tvOptionOne?.text = question.optionOne
         tvOptionTwo?.text = question.optionTwo
         tvOptionThree?.text = question.optionThree
         tvOptionFour?.text = question.optionFour
+
+        if(mCurrentPosition == mQuestionList!!.size){
+            btnSubmit?.text = "FINISH"
+        }else{
+            btnSubmit?.text = "SUBMIT"
+
+        }
+    }
+
+    //선택이 되지 않았을 때 보여지는 뷰
+    private fun defaultOptionsView(){
+        val options = ArrayList<TextView>()
+        tvOptionOne?.let{
+            options.add(0, it)
+        }
+        tvOptionTwo?.let{
+            options.add(1, it)
+        }
+        tvOptionThree?.let{
+            options.add(2, it)
+        }
+        tvOptionFour?.let{
+            options.add(3, it)
+        }
+        for(option in options){
+            option.setTextColor(Color.parseColor("#7A8089"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(this, R.drawable.default_option_border_bg)
+
+        }
+    }
+
+    //option이 눌렸을 때 보여지는 것
+    private fun selectedOptionView(tv:TextView, selectedOptionNum:Int){
+        defaultOptionsView()
+        mSelectedOptionPosition = selectedOptionNum
+        tv.setTextColor(Color.parseColor("#363a43"))
+        tv.setTypeface(tv.typeface, Typeface.BOLD)
+        tv.background = ContextCompat.getDrawable(this, R.drawable.selected_option_border_bg)
+    }
+
+    //option 버튼이 눌렸을 때 동작
+    override fun onClick(view: View?) {
+
+        when(view?.id){
+            R.id.tv_option_one -> {
+                tvOptionOne?.let{
+                 selectedOptionView(it,1)
+                }
+            }
+            R.id.tv_option_two -> {
+                tvOptionTwo?.let{
+                    selectedOptionView(it,2)
+                }
+            }
+            R.id.tv_option_three -> {
+                tvOptionThree?.let{
+                    selectedOptionView(it,3)
+                }
+            }
+            R.id.tv_option_four -> {
+                tvOptionFour?.let{
+                    selectedOptionView(it,4)
+                }
+            }
+            R.id.btn_submit -> {
+
+
+            }
+
+        }
     }
 }
